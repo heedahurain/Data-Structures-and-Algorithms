8cea7b2228c6fa57fb4db487fcb70119837d1a42
Heeda Hurain Siddiqui <heeda.hurain21@gmail.com>
2026-08-27 20:06:36 +0530

    Star Pattern using Nested For Loops
    print a 4x4 square star pattern

--- star_pattern.cpp
+++ star_pattern.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    for (int i = 0; i<4; i++){
-        for (int j=0; j<4; j++){
-            cout<<"*";
+void print1(int n) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n ; j++) {
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
     }
+}
+int main() {
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    print1(n);
     return 0;
 }
