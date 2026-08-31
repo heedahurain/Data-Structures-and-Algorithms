8e678d659320ed9a1bbbaaa47cdcf5d6018ad4a9
Heeda Hurain Siddiqui <heeda.hurain21@gmail.com>
2026-08-31 12:55:24 +0530

    Decreasing Patterns

--- decreasing_number_pattern.cpp
+++ decreasing_number_pattern.cpp
@@ -1,18 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
- 
-void print6(int n){
-    for (int i = 1 ; i<=n ; i++){
-        for ( int j=0 ; j<n-i+1 ; j++){
-            cout<<i<<" ";
+
+void print6(int n) {
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n - i + 1; j++) {
+            cout << j << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
-int main(){
-    int n ; 
-    cout<<"enter no. of rows: ";
-    cin>>n;
+
+int main() {
+    int n;
+
+    cout << "Enter number of rows: ";
+    cin >> n;
+
     print6(n);
+
     return 0;
 }
